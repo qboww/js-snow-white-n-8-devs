d5d66eb4b4946d718e1fcbcb865804f71fb98b38
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-04-18 12:19:52 +0200

    added section 'reviews'. html, css,js

---
+++ src/js/render-reviews.js
@@ -0,0 +1,13 @@
+const renderReview = ({ _id, avatar_url, author, review }) => {
+  return `<li class="reviews-list-item swiper-slide" id="${_id}">
+            <img class="thumbnail" src="${avatar_url}" alt="${author}" width="48" height="48" loading="lazy">
+            <div class="opinion">
+              <h3 class="author">${author}</h3>
+              <p class="review">${review.trim()}</p>
+            </div>
+          </li>`;
+};
+
+export function renderReviews(reviews) {
+  return reviews.map(renderReview).join('');
+}
