9782f1630f2594ced60a58fbae5cd028a368898d
Valentyn-Melnyk <v.iv.melnyk@gmail.com>
2024-04-19 21:01:43 +0300

    feature-back-to-top-scroll

--- src/js/back-to-top.js
+++ src/js/back-to-top.js
@@ -21,4 +21,20 @@ export function backToTop() {
   };
 
   backToTopButton.addEventListener('click', goToTop);
+
+  const pageProgressBar = document.querySelector('.progress-bar');
+  document.addEventListener('scroll', () => {
+    const scrolledPercentage =
+      (scrollContainer().scrollTop /
+        (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+      100;
+
+    pageProgressBar.style.width = `${scrolledPercentage}%`;
+
+    if (scrollContainer().scrollTop > showOnPx) {
+      backToTopButton.classList.remove('hidden');
+    } else {
+      backToTopButton.classList.add('hidden');
+    }
+  });
 }
