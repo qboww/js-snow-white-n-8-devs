0a7ef69d0cceaab55cbacb2a89fe14498b8a06d2
Valentyn-Melnyk <v.iv.melnyk@gmail.com>
2024-04-19 17:50:09 +0300

    feature-back-to-top

---
+++ src/js/back-to-top.js
@@ -0,0 +1,24 @@
+export function backToTop() {
+  const showOnPx = 100;
+  const backToTopButton = document.querySelector('.back-to-top');
+
+  const scrollContainer = () => {
+    return document.documentElement || document.body;
+  };
+
+  document.addEventListener('scroll', () => {
+    if (scrollContainer().scrollTop > showOnPx) {
+      backToTopButton.classList.remove('hidden');
+    } else {
+      backToTopButton.classList.add('hidden');
+    }
+  });
+
+  const goToTop = () => {
+    document.body.scrollIntoView({
+      behavior: 'smooth',
+    });
+  };
+
+  backToTopButton.addEventListener('click', goToTop);
+}
